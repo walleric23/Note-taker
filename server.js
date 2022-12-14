426a44d9daf8ba0b1870c1da2c8e15906b2322af
walleric23 <walleric23@gmail.com>
2022-12-13 20:36:30 -0800

    code comments

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// imported packages
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const fs = require("fs");
+// package for unique IDs
 const uniqid = require("uniqid");
 const app = express();
 
@@ -11,20 +13,21 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-// GET Route for landing page
+// GET Route for notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
-
+// GET Route for landing page
 app.get("/api/notes", (req, res) =>
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
     res.send(data);
   })
 );
-
+// POST route for the saved notes
 app.post("/api/notes", (req, res) =>
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
     const notes = JSON.parse(data);
+    // pushes them and assigns random ID
     notes.push({
       id: uniqid(),
       ...req.body,
@@ -34,7 +37,7 @@ app.post("/api/notes", (req, res) =>
     });
   })
 );
-
+// removes note with matching ID and returns an updated array without deleted note
 app.delete("/api/notes/:id", (req, res) =>
   fs.readFile("./db/db.json", "utf-8", (err, data) => {
     const notes = JSON.parse(data);
@@ -48,7 +51,7 @@ app.delete("/api/notes/:id", (req, res) =>
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
-
+// port listening @ 3001
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
